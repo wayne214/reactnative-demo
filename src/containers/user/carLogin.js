0349205644dd956072d855073bd366841d764284
liqingqing <qingqingyeshi311@163.com>
2017-09-14 16:05:01 +0800

    add

--- src/containers/user/carLogin.js
+++ src/containers/user/carLogin.js
@@ -134,7 +134,7 @@ class LoginContainer extends BaseComponent {
 								style={ styles.input }
 								multiline={ false }
 								placeholder='请输入手机号'
-								keyboardType={ 'phone-pad' }
+								keyboardType='numeric'
 								value={ this.state.username }
 								underlineColorAndroid={ 'transparent' }
 								onChangeText={ (text) => this.setState({ username: text }) }/>
@@ -147,7 +147,7 @@ class LoginContainer extends BaseComponent {
                     } }
                     >
                     <Text
-                      style={ styles.iconFontRight }>&#xe613;</Text>
+                      style={ styles.iconFontRight }>&#xe634;</Text>
                   </TouchableOpacity>
                 </View>
 						</View>
