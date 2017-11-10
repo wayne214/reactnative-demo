1da7ce5f0f6da9b7a92e7602088b073cb2458025
wanglei <wanglei1005@qq.com>
2017-11-10 15:25:26 +0800

    修改bug

--- src/components/common/codeDialog.js
+++ src/components/common/codeDialog.js
@@ -167,7 +167,7 @@ const styles = StyleSheet.create({
         backgroundColor: 'white',
         // borderRadius: 6,
         paddingTop: 30,
-        marginBottom: 135,
+        marginBottom: height/30*11,
     },
 
     cellContainer: {
