84c924675a6ac20c96254ae7238f7b99dc786ffe
王军伟 <wayne214@163.com>
2018-03-17 10:50:01 +0800

    合同模板

--- src/constants/setting.js
+++ src/constants/setting.js
@@ -32,7 +32,7 @@ export const OSS_ADD_DRIVER = 'driver_license';
 export const OSS_ADD_CAR = 'car';
 
 
-export const CONTRACT_TEMPLATE_URL = DEBUG ? 'http://file-test.lenglianmajia.com/contract_template/template_carrier.pdf' : 'http://file.lenglianmajia.com/contract_template/template_carrier.pdf'
+export const CONTRACT_TEMPLATE_URL = DEBUG ? 'http://paas-web-test.xianyiscm.com/inf/template_carrier.pdf' : 'http://file.lenglianmajia.com/contract_template/template_carrier.pdf'
 
 // export const XYT_HOST = DEBUG ? 'http://mproxy-beta.xianyiscm.com/' : 'http://mproxy.xianyiscm.com/';
 export const XYT_HOST = DEBUG ? 'http://mproxy-test.xianyiscm.com/' : 'http://mproxy.xianyiscm.com/';
