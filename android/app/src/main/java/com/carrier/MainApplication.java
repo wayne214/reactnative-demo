a2c730cb173ba6120442af2ff20dbbaeb568613c
席智欣 <947538363@qq.com>
2018-03-17 13:31:14 +0800

    修改bug

--- android/app/src/main/java/com/carrier/MainApplication.java
+++ android/app/src/main/java/com/carrier/MainApplication.java
@@ -54,7 +54,7 @@ public class MainApplication extends Application implements ReactApplication {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
             new RNCameraPackage(),
-            new BaiduMapPackage(),
+              new BaiduMapPackage(getApplicationContext()),
             new RCTToastPackage(),
             new PickerViewPackage(),
             new ImagePickerPackage(),
