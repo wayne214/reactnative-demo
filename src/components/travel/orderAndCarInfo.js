2d81fc670cc06a34026e2f51ab06acb00f12fc97
王军伟 <wayne214@163.com>
2018-04-19 09:43:19 +0800

    数据判空

--- src/components/travel/orderAndCarInfo.js
+++ src/components/travel/orderAndCarInfo.js
@@ -34,30 +34,30 @@ class orderAndCarInfo extends Component{
         const carInfo = <View style={{backgroundColor: 'transparent', marginLeft: 5}}>
             <View style={{flexDirection: 'row', }}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>车  牌  号：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.vehical}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.vehical ? infoData.vehical : ''}</Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: 10,}}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>司        机：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.driver}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.driver ? infoData.driver : ''}</Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: 10}}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>车辆类型：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.carType}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.carType ? infoData.carType : ''}</Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: 10}}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>承  运  商：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.carrierName}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.carrierName ? infoData.carrierName : ''}</Text>
             </View>
         </View>;
 
         const orderInfo = <View style={{backgroundColor: 'transparent', marginLeft: 5}}>
             <View style={{flexDirection: 'row', }}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>创建时间：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.createTime}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.createTime ? infoData.createTime : ''}</Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: 10}}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>发车时间：</Text>
-                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.outboundTime}</Text>
+                <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.outboundTime ? infoData.outboundTime : ''}</Text>
             </View>
             <View style={{flexDirection: 'row', marginTop: 10,}}>
                 <Text style={{color: '#ffffff', fontSize: 14}}>业务类型：</Text>
@@ -66,11 +66,11 @@ class orderAndCarInfo extends Component{
             <View style={{flexDirection: 'row', marginTop: 10,}}>
                 <View style={{flexDirection: 'row', }}>
                     <Text style={{color: '#ffffff', fontSize: 14}}>配  送  点：</Text>
-                    <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.orderNumber}</Text>
+                    <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.orderNumber ? infoData.orderNumber : ''}</Text>
                 </View>
-                <View style={{flexDirection: 'row',}}>
+                <View style={{flexDirection: 'row', marginLeft: 10}}>
                     <Text style={{color: '#ffffff', fontSize: 14}}>装载率：</Text>
-                    <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.loadRate}</Text>
+                    <Text style={{color: '#ffffff', fontSize: 14}}>{infoData.loadRate ? infoData.loadRate : ''}</Text>
                 </View>
             </View>
         </View>;
