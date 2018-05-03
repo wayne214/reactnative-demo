1c3bcdfacb13af6dbc57dfe0f17084ce658fafaf
王军伟 <wayne214@163.com>
2018-05-03 16:57:51 +0800

    优化

--- src/containers/order/historyOrders/searchHistoryOrder.js
+++ src/containers/order/historyOrders/searchHistoryOrder.js
@@ -25,12 +25,13 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
     },
-    timeContainer: {
+    itemContainer: {
         height: 44,
         flexDirection: 'row',
         alignItems: 'center',
         justifyContent: 'space-between',
         paddingHorizontal: 15,
+        backgroundColor: '#fff'
     },
     timeTitle: {
         fontSize: 15,
@@ -49,12 +50,22 @@ const styles = StyleSheet.create({
         fontSize: 17,
         color: StaticColor.WHITE_COLOR,
     },
+    noticeIcon:{
+        fontFamily: 'iconfont',
+        fontSize: 14,
+        color: '#ff8500',
+        alignSelf: 'center',
+        paddingLeft: 5,
+        paddingRight: 5,
+        paddingBottom: 10,
+    },
 });
 class searchHistoryOrder extends Component {
     constructor(props) {
         super(props);
+        this.showDatePick = this.showDatePick.bind(this);
     }
-    showDatePick() {
+    showDatePick(type) {
         TimePicker.init({
             pickerConfirmBtnText: '确定',
             pickerCancelBtnText: '取消',
@@ -63,7 +74,7 @@ class searchHistoryOrder extends Component {
             pickerFontSize: 22,
             pickerBg: [225,225,225,1],
             onPickerConfirm: data => {
-                this._onPickerConfirm(data)
+                console.log('选择的时间', data);
 
             },
             onPickerCancel: data => {
@@ -78,28 +89,32 @@ class searchHistoryOrder extends Component {
     render() {
         const {historyOrders} = this.props;
         const searchView= <View>
-            <View style={{height: 65, backgroundColor: '#FFFAF4', justifyContent: 'center', paddingHorizontal: 15}}>
-                <Text style={{fontSize: 14, color: '#FF8500', lineHeight: 20}}>
+            <View style={{padding: 10, backgroundColor: '#FFFAF4',flexDirection: 'row'}}>
+                <Text style={styles.noticeIcon}>&#xe69c;</Text>
+                <Text style={{fontSize: 14, color: '#666666', marginLeft: 5, marginRight: 20, paddingTop: 5, paddingBottom: 5,}}>
                     仅限查询2018年4月1日前订单，按照订单生成时间查询
                 </Text>
             </View>
-            <TouchableOpacity onPress={()=>{
+            <TouchableOpacity activeOpacity={0.5} onPress={()=>{
                 // 选择时间弹框
+                this.showDatePick(0)
             }}>
-                <View style={styles.itemContainer}>
+                <View style={[styles.itemContainer, {marginTop: 10}]}>
                     <Text style={styles.timeTitle}>起始时间</Text>
-                    <Text style={{fontSize: 20, color: '#666666'}}>&#xe63d;</Text>
+                    <Text style={{fontSize: 17, color: '#c7c7c7', fontFamily: 'iconfont'}}>&#xe63d;</Text>
                 </View>
             </TouchableOpacity>
-            <TouchableOpacity onPress={()=>{
+            <View style={{backgroundColor: '#E6EAF2', height: 0.5}}/>
+            <TouchableOpacity activeOpacity={0.5} onPress={()=>{
                 // 选择时间弹框
+                this.showDatePick(1)
             }}>
                 <View style={styles.itemContainer}>
                     <Text style={styles.timeTitle}>截止时间</Text>
-                    <Text style={{fontSize: 20, color: '#666666'}}>&#xe63d;</Text>
+                    <Text style={{fontSize: 17, color: '#c7c7c7', fontFamily: 'iconfont'}}>&#xe63d;</Text>
                 </View>
             </TouchableOpacity>
-            <View>
+            <View style={{marginTop: 44}}>
                 <TouchableOpacity
                     onPress={() => {
                         // 搜索按钮
@@ -118,18 +133,21 @@ class searchHistoryOrder extends Component {
                     title={ '历史订单' }
                     router={this.props.navigation}
                 />
-                <HistoryList
-                    dataSource={historyOrders}
-                    itemClick={(rowData)=>
-                        this.props.navigation.dispatch({
-                            type: RouteType.ROUTE_ORDER_DETAIL,
-                            params: {deliveryno: rowData.deliveryno, orderSource: rowData.state == '10' ? 2 : rowData.orderSource, orderStatus: rowData.state},
+                {
+                    1 == 1 ? searchView : <HistoryList
+                        dataSource={historyOrders}
+                        itemClick={(rowData)=>
+                            this.props.navigation.dispatch({
+                                type: RouteType.ROUTE_ORDER_DETAIL,
+                                params: {deliveryno: rowData.deliveryno, orderSource: rowData.state == '10' ? 2 : rowData.orderSource, orderStatus: rowData.state},
 
-                        })
-                    }
-                    refreshList={()=> console.log('刷新')}
-                    loadMore={() => console.log('加载更多')}
-                />
+                            })
+                        }
+                        refreshList={()=> console.log('刷新')}
+                        loadMore={() => console.log('加载更多')}
+                    />
+                }
+
             </View>
         )
 
